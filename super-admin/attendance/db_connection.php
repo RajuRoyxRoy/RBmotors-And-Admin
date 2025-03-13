a2cd0295a45b7e96bcfbb7ec54ad2218d8c5cfa4
Raju Rajbongshi <prietyroyx@gmail.com>
2025-03-13 22:54:28 +0530

    attendance files

---
+++ super-admin/attendance/db_connection.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root"; // default username for XAMPP/WAMP
+$password = ""; // default password for XAMPP/WAMP
+$dbname = "client_data";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
